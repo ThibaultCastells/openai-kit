9dde780684b03f79297fed5583e61bc50c4667ff
Thibault Castells <thibault.castells@wanadoo.fr>
2023-03-17 20:50:46 +0900

    http to git

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.13.0"),
+        .package(url: "git@github.com:swift-server/async-http-client.git", from: "1.13.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
